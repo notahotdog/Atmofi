91bfa699eda397d5daf82520ad6958ae616c7b5d
ray-97 <something.ray97@gmail.com>
2025-06-20 14:02:33 +0800

    feat: add alchemy-sdk and dotenv dependencies; implement deploy script for Atmofi contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-// import "@chainlink/hardhat-chainlink";
+import "dotenv/config"; // Import dotenv
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {}, // The local development network
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+  },
 };
 
 export default config;
---
+++ scripts/deploy.ts
@@ -0,0 +1,23 @@
+// scripts/deploy.ts
+
+import { ethers } from "hardhat";
+
+async function main() {
+  // The live address of the SOL/USD price feed on the Sepolia testnet
+  const chainlinkFeedAddress = "0x694AA1769357215DE4FAC081bf1f309aDC325306";
+
+  console.log("Deploying Atmofi contract...");
+
+  const atmofi = await ethers.deployContract("Atmofi", [chainlinkFeedAddress]);
+
+  await atmofi.waitForDeployment();
+
+  console.log(`Atmofi contract deployed to: ${await atmofi.getAddress()}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+// Atmofi contract deployed to: 0xF8346e23BdE55C7692219a54CA95432E66046574
